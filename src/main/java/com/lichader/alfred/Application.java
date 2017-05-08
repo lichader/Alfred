230348f6283f8a045850446c2bbabaf249052e5c
Lichader <pengyu.7@gmail.com>
2017-05-09 00:57:58 +1000

    add application class file

---
+++ src/main/java/com/lichader/alfred/Application.java
@@ -0,0 +1,14 @@
+package com.lichader.alfred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lichader on 9/5/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main (String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
